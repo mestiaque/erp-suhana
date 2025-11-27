841eeaa843c3a71369cdb7d2d43e498b4b7198d7
rabiulkarim <rabiulk449@gmail.com>
2025-11-27 18:19:09 +0600

    user log and active status update

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -81,6 +81,21 @@ class AuthController extends Controller
                             'last_active_at' => now()->toDateTimeString(),
                         ]),
                     ]);
+                    ActivityLog::create([
+                        'uuid' => Str::uuid()->toString(),
+                        'event' => 'user_active',
+                        'title' => "User Active: {$user->name}",
+                        'user_type' => get_class($user),
+                        'user_id' => $user->id,
+                        'loggable_type' => get_class($user),
+                        'loggable_id' => $user->id,
+                        'data' => json_encode([
+                            'last_active_at' => now()->toDateTimeString(),
+                            'ip' => $r->ip(),
+                            'user_agent' => $r->userAgent(),
+                            'url' => $r->fullUrl(),
+                        ]),
+                    ]);
 
                     $redirect =Session::get('url.intended');
                     //Session::forget('url.intended');
