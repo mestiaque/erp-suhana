5850d371186f6e06d1c167fe54ba8602c46b9e1c
khan1998 <mrm.khan.1298@gmail.com>
2026-04-18 15:23:40 +0600

    bug fix

--- config/hr.php
+++ config/hr.php
@@ -3,8 +3,4 @@
 return [
     // ...other HR config...
 
-    // Helper to get all HR options (for controllers/views)
-    'options' => function() {
-        return \App\Services\HrOptionsService::getOptions();
-    },
 ];
