2bf9028d484693eae09ab80a3586426e4009a21f
khan1998 <mrm.khan.1298@gmail.com>
2026-07-30 16:43:03 +0600

    my-profile-ok

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -62,7 +62,7 @@ class AdminController extends Controller
         $todayLogins = ActivityLog::
             where('user_type', User::class)
             ->whereDate('created_at', today())
-            ->count();
+            ->count('user_id');
 
         $chartData = collect();
         $dailyLogins = ActivityLog::
